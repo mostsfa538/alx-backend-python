02c3c87b356f147b1aa170ef83675a68114145bd
mostsfa538 <mostafa51moktar@gmail.com>
2024-08-01 15:36:05 +0300

    6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" sum_mixed_list function """
+from typing import List, Union
+
+
+def sum_mixed_list(arr: List[Union[int, float]]) -> float:
+    """ return sum_mixed_list """
+    return sum(arr)
