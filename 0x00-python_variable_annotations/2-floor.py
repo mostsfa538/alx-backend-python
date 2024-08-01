f79e9756acca1dabfca539dd9d43e4b9d7d0e2b7
mostsfa538 <mostafa51moktar@gmail.com>
2024-08-01 15:18:37 +0300

    2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import math
 """ floor function """
+import math
 
 
 def floor(n: float) -> int:
     """ return the floor of n """
-    return math.floor(n)
+    return (math.floor(n))
