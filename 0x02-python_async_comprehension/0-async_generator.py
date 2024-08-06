eca770d07527d359f061b60dd31021d08b1cb0ab
mostsfa538 <mostafa51moktar@gmail.com>
2024-08-06 20:31:15 +0300

    0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+""" .... """
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """ .... """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
